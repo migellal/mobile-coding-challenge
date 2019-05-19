8f1e8359a9e63ba8c14cd96525b8e1d3ca8eb4c2
migellal <michalgellert@gmail.com>
2019-05-19 13:54:24 +0200

    Add Recycler View

--- app/src/main/java/pl/michalgellert/trendingrepos/MainActivity.kt
+++ app/src/main/java/pl/michalgellert/trendingrepos/MainActivity.kt
@@ -3,19 +3,18 @@ package pl.michalgellert.trendingrepos
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_trending -> {
-                textMessage.setText(R.string.title_trending)
+                message_tv.setText(R.string.title_trending)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_settings -> {
-                textMessage.setText(R.string.title_settings)
+                message_tv.setText(R.string.title_settings)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -24,9 +23,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        textMessage = findViewById(R.id.message)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        navView_bnv.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
