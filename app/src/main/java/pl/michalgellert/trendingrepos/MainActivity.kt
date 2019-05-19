70886d9bc746560ee8e0f50cef0a55cee127625c
migellal <michalgellert@gmail.com>
2019-05-19 14:49:10 +0200

    Add Recycler View Impl & fake data

--- app/src/main/java/pl/michalgellert/trendingrepos/MainActivity.kt
+++ app/src/main/java/pl/michalgellert/trendingrepos/MainActivity.kt
@@ -1,9 +1,12 @@
 package pl.michalgellert.trendingrepos
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import pl.michalgellert.trendingrepos.model.Repository
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,18 +14,36 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.navigation_trending -> {
                 message_tv.setText(R.string.title_trending)
+                downloadData()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_settings -> {
                 message_tv.setText(R.string.title_settings)
+                repositoryList_rv.visibility = View.INVISIBLE
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navView_bnv.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        downloadData()
     }
+
+    private fun downloadData() {
+        repositoryList_rv.apply {
+            visibility = View.VISIBLE
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = RepositoryListAdapter(fakeData())
+        }
+    }
+
+    fun fakeData(): List<Repository> = listOf(
+        Repository("repo1", "repo1 desc", 123456, "user", "https://avatars0.githubusercontent.com/u/17506342?v=4"),
+        Repository("repo2", "repo2 desc", 1, "user2", "https://avatars0.githubusercontent.com/u/17506342?v=4"),
+        Repository("repo3", "repo3 desc", 99, "user3", "https://avatars0.githubusercontent.com/u/17506342?v=4")
+    )
 }
---
+++ app/src/main/java/pl/michalgellert/trendingrepos/RepositoryListAdapter.kt
@@ -0,0 +1,43 @@
+package pl.michalgellert.trendingrepos
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import pl.michalgellert.trendingrepos.model.Repository
+
+class RepositoryListAdapter(private val list: List<Repository>)
+    : RecyclerView.Adapter<RepositoryViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return RepositoryViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: RepositoryViewHolder, position: Int) {
+        val repo: Repository = list[position]
+        holder.bind(repo)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+}
+
+class RepositoryViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.repository_list_item, parent, false)) {
+    private var repoName: TextView = itemView.findViewById(R.id.repoName_tv)
+    private var repoDescription: TextView = itemView.findViewById(R.id.repoDescription_tv)
+    private var repoAvatar: ImageView = itemView.findViewById(R.id.avatar_iv)
+    private var repoOwner: TextView = itemView.findViewById(R.id.repoOwner_tv)
+    private var repoStars: TextView = itemView.findViewById(R.id.repoStars_tv)
+
+    fun bind(repository: Repository) {
+        repoName.text = repository.name
+        repoDescription.text = repository.description
+        // repoAvatar.setImageBitmap() TODO Picasso here
+        repoOwner.text = repository.username
+        repoStars.text = repository.stars.toString()
+    }
+
+}
